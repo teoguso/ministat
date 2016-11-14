ba3c155bac4dfec51aa072d6d37bd11c9425e366
Matteo Guzzo <guzzo.matteo@gmail.com>
2016-11-14 17:41:03 +0100

    Added a couple more functions.

--- src/stat_functions.py
+++ src/stat_functions.py
@@ -13,7 +13,7 @@ def mean(f_list):
     return mysum / len(f_list)
 
 
-def variance(f_list):
+def var(f_list):
     """
     Computes the variance of the elements of a given list of numbers
     :param f_list:
@@ -34,5 +34,22 @@ def stddev(f_list):
     """
     return sqrt(variance(f_list))
 
+
+def cov(f_list1, f_list2):
+    """
+    Computes the covariance between two given list of numbers
+    :param f_list1:
+    :param f_list2:
+    :return:
+    """
+    mysum = 0.
+    mu1, mu2 = mean(f_list1), mean(f_list2)
+    for x, y in zip(f_list1, f_list2):
+        mysum += (x - mu1) * (y - mu2)
+    return mysum / len(f_list1)
+
+
+def pcc(f_list1, f_list2):
+    return cov(f_list1, f_list2) / sqrt(var(f_list1) * var(f_list2))
 
 
